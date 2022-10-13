62f4323a7e9c1ae3883e48d839059a38ba2e66e0
Misha Tuesday <mishatuesday@gmail.com>
2022-10-13 15:37:23 -0400

    add search function

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,23 +5,29 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchTerm, setSearchTerm] = useState("")
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
+  function handleSearchChange(event) {
+    setSearchTerm(event.target.value)
+  }
   const itemsByFilter = items.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
   });
 
-  const itemsToDisplay = itemsByFilter;
+  const itemsToDisplay = itemsByFilter.filter((item) => {
+    return item.name.toLowerCase().includes(searchTerm.toLowerCase())
+  });
 
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleSearchChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
